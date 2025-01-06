e8c9b06408656903845fd9b98426576b386dbc25
Ahamed1846 <ahamed1062005@gmail.com>
2025-01-06 20:35:07 +0530

    BOCS Project

--- Project4.1/Mult.asm
+++ Project4.1/Mult.asm
@@ -5,31 +5,33 @@
 
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
+//
+// This program only needs to handle arguments that satisfy
+// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+
+// Put your code here.
 
 @sum
-M=0 //sum=0
+M=0   //sum=0
 @i
-M=1 //i=1
+M=1   //i=1
 (LOOP)
-@i
-D=M //D=i
+@i    
+D=M   //D=i
 @R0
 D=D-M //D=i-R0
 @END
-D;JGT // if(i-R0) > 0 goto END
+D;JGT  // if(i-R0) > 0 goto END
 @R1
-D=M //D=R1
+D=M   //D=R1
 @sum
-M=D+M //sum+=R1
+M=D+M  //sum+=R1
 @i
-M=M+1 //i=i+1
+M=M+1  //i=i+1
 @LOOP
-0;JMP //Goto LOOP
+0;JMP  //Goto LOOP
 (END)
-@sum //set final result to R2
+@sum
 D=M
 @R2
 M=D
-@REPEAT
-(REPEAT)
-0;JMP  //infinite loop at program end
